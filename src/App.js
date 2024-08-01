927caf7fdae8174c0aaf037b30328fcd61d02e1f
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2024-08-01 15:06:45 +0530

    removed routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
 import './index.css';
-import Login from './pages/Admin/AdminLogin';
 import Home from './pages/Home/index';
-import Admin from './pages/Admin/index';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path='/admin' element={<Admin/>}/>
-        <Route path='/admin-login' element={<Login/>}/>
-      </Routes>
-    </Router>
+   <Home/>
   );
 }
 

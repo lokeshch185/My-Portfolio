69693a913767ef3028d1578f02f972f18c7402c4
lokesh chaudhari <lokeshchaudhari14@gmail.com>
2024-08-08 00:42:20 +0530

    updated github stlying

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -4,7 +4,7 @@ function About({data}) {
     return (
         <div className="min-h-screen w-screen bg-[#1b5982] flex justify-center items-center">
             <div className="flex w-full  h-auto items-center sm:flex-col  p-8">
-                <div className="w-1/2 sm:w-full  flex  justify-evenly">
+                <div className="w-1/2 sm:w-full  flex  justify-evenly pt-2">
                     <img src={'/profilepicture.jpg' || data.imgUrl} alt="Profile" className="rounded-full shadow-xl w-64 h-64 scale-125  object-cover" />
                 </div>
                 <div className="flex flex-col w-1/2 sm:w-full sm:mt-8 sm:text-center px-2 sm:py-6 gap-10">
